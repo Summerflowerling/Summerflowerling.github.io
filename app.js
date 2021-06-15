ed579e117deb43b53ffae127a470af24bf97622e
yuling <yulinglin@protonmail.com>
2021-06-15 19:25:58 +0800

    refactor: change a variable name in app.js
    Changed the name closeTheModal to modalCloseBtn to specify it is a button. Also cleaned the space lines.

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 
-
-
+// Ｇlobal variable
 let modal = document.getElementById("modal")
-let closeTheMadal = document.getElementById("close")
+let modalCloseBtn = document.getElementById("close")
 let modalImg = document.getElementById("modalImg")
 let modalImgTitle = document.getElementById("modal-img-title")
 
-
-
-
-
-
-
-
 function displayModal(imgSrc, imgTitle){
-  
-  modal.style.display="block"
-  closeTheMadal.style.display="block";
-  modalImg.src = imgSrc
-  console.log(imgTitle)
-  modalImgTitle.innerHTML = imgTitle
-
+    modal.style.display="block"
+    modalCloseBtn.style.display="block";
+    modalImg.src = imgSrc
+    console.log(imgTitle)
+    modalImgTitle.innerHTML = imgTitle
 }
 
-closeTheMadal.addEventListener("click", close)
 function close(){
-    closeTheMadal.style.display="none"
+    modalCloseBtn.style.display="none"
     modal.style.display="none"
 }
+
+modalCloseBtn.addEventListener("click", close)
