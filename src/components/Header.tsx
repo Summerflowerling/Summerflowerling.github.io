8a7804c23b87d8cb7340dd778ddc23cebb48cf52
phoebeyllin <PhoebeYLLin@mobiledrivetech.com>
2025-07-10 14:49:17 +1000

    Update: update header behaviour

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,45 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 
 const Header = () => {
   const location = useLocation();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll);
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
 
   return (
-    <div>
-      <div className={styles.navBox}>
-        <h1 className={styles.logo}>Yuling</h1>
-        <ul className={styles.navList}>
-          <li
-            className={location.pathname === '/about' ? styles.currentPage : ''}
-          >
-            <Link to='/about'>About me</Link>
-          </li>
-          <li
-            className={
-              location.pathname === '/gallery' ? styles.currentPage : ''
-            }
-          >
-            <Link to='/gallery'>Gallery</Link>
-          </li>
-          <li
-            className={
-              location.pathname === '/contact' ? styles.currentPage : ''
-            }
-          >
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </div>
+    <div className={`${styles.navBox} ${scrolled ? styles.scrolled : ''}`}>
+      <h1 className={styles.logo}>Yuling</h1>
+      <ul className={styles.navList}>
+        <li
+          className={location.pathname === '/about' ? styles.currentPage : ''}
+        >
+          <Link to='/about'>About me</Link>
+        </li>
+        <li
+          className={location.pathname === '/gallery' ? styles.currentPage : ''}
+        >
+          <Link to='/gallery'>Gallery</Link>
+        </li>
+        <li
+          className={location.pathname === '/contact' ? styles.currentPage : ''}
+        >
+          <Link to='/contact'>Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
