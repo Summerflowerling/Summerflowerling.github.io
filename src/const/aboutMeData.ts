1276d8300b9a0c7f791d117ea5898ca410953af3
phoebeyllin <PhoebeYLLin@mobiledrivetech.com>
2025-09-02 22:06:43 +1000

    fix: update content to British English

--- src/const/aboutMeData.ts
+++ src/const/aboutMeData.ts
@@ -3,7 +3,7 @@ export const workExperiences = [
     id: 1,
     title: 'Software Engineer - Health Tech',
     description:
-      'Developed AI-powered tools to enhance patient retention for dental clinics, crafting React and TypeScript interfaces for personalized outreach and seamless data workflows. Integrated secure APIs with fuzzy search and role-based access, boosting engagement with 80%+ test coverage.',
+      'Developed AI-powered tools to enhance patient retention for dental clinics, crafting React and TypeScript interfaces for personalised outreach and seamless data workflows. Integrated secure APIs with fuzzy search and role-based access, boosting engagement with 80%+ test coverage.',
     years: 'Feb 2025 - Present',
     colorClass: 'aboutMeWidgetColor2',
   },
@@ -27,7 +27,7 @@ export const workExperiences = [
     id: 4,
     title: 'Helpline Volunteer',
     description:
-      'Listened and supported callers with empathy, shaping my knack for human-centered solutions over five years.',
+      'Listened and supported callers with empathy, shaping my knack for human-centred solutions over five years.',
     years: 'Feb 2017 - Feb 2022',
     colorClass: 'aboutMeWidgetColor1',
   },
@@ -48,7 +48,7 @@ export const books = [
     subtitle: 'Tess Gerritsen',
     image: '/img/the-silient-girl-cover.jpg',
     review:
-      'This gripping thriller, part of my favorite mystery novel series by my top suspense writer, has a fascinating plot, well-developed characters, and a wealth of medical knowledge. I couldn’t put it down!\n',
+      'This gripping thriller, part of my favourite mystery novel series by my top suspense writer, has a fascinating plot, well-developed characters, and a wealth of medical knowledge. I couldn’t put it down!\n',
     amazonLink:
       'https://www.amazon.com/Silent-Girl-Rizzoli-Isles-Novel/dp/034551551X',
   },
--- src/const/storyData.ts
+++ src/const/storyData.ts
@@ -2,7 +2,7 @@ export const storyContent = {
   firstSentence:
     "From firing up kids' interest in code back in Taiwan to tinkering with health tech apps in Sydney...",
   restOfStory:
-    "I kicked things off as a STEM instructor, turning tricky tech into kid-friendly lessons while building a few async apps on the side. That path took me deeper into engineering, where I crafted React Native tools that boosted user engagement and whipped up quick prototypes alongside international teams. These days, I'm using React and TypeScript to create AI-powered tools that foster warm, human-like patient interactions, boosting retention through thoughtful, personalized experiences. Fluent in English, Japanese, and Mandarin, with helpline volunteer work sharpening my empathy and problem-solving skills, I thrive on the human side of tech, like listening to needs and coding solutions that stick.",
+    "I kicked things off as a STEM instructor, turning tricky tech into kid-friendly lessons while building a few async apps on the side. That path took me deeper into engineering, where I crafted React Native tools that boosted user engagement and whipped up quick prototypes alongside international teams. These days, I'm using React and TypeScript to create AI-powered tools that foster warm, human-like patient interactions, boosting retention through thoughtful, personalised experiences. Fluent in English, Japanese, and Mandarin, with helpline volunteer work sharpening my empathy and problem-solving skills, I thrive on the human side of tech, like listening to needs and coding solutions that stick.",
   lastWords:
     '\n Eager to dive into more AI-enhanced tools and team up on bigger projects soon.',
   keyPhrases: [
